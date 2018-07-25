c1e6ea5f30193a3b919887e272a7843e50a3e2db
Madison May <madison@indico.io>
2018-07-25 12:49:16 -0400

    WIP: refactor target_type logic and subclasses

--- finetune/sequence_labeling.py
+++ finetune/sequence_labeling.py
@@ -1,32 +1,11 @@
-from finetune.base import BaseModel, SEQUENCE_LABELING
+from finetune.base import BaseModel
 from finetune.encoding import EncodedOutput, ArrayEncodedOutput
 from finetune.target_encoders import SequenceLabelingEncoder
+from finetune.network_modules import sequence_labeler
 from finetune.utils import indico_to_finetune_sequence, finetune_to_indico_sequence
 
 
 class SequenceLabeler(BaseModel):
-
-    def _text_to_ids_with_labels(self, X, Y=None):
-        # X: list of list of text snippets.  Each snippet represents a segment of text with a consistent label
-        encoder_out = self.encoder.encode_sequence_labeling(X, Y=Y, max_length=self.config.max_length)
-        return self._array_format(encoder_out)
-
-    def _finetune(self, X, Y, batch_size=None):
-        """
-        X: List / array of text
-        Y: Class labels
-        val_size: Float fraction or int number that represents the size of the validation set.
-        val_interval: The interval for which validation is performed, measured in number of steps.
-        """
-        arr_encoded = self._text_to_ids_with_labels(X, Y=Y)
-        return self._training_loop(
-            arr_encoded,
-            Y=arr_encoded.labels,
-            batch_size=batch_size or self.config.batch_size
-        )
-
-    def _get_target_encoder(self):
-        return SequenceLabelingEncoder()
 
     def finetune(self, X, Y, batch_size=None):
         """
@@ -39,8 +18,12 @@ class SequenceLabeler(BaseModel):
         :param val_interval: The interval for which validation is performed, measured in number of steps.
         """
         X, Y = indico_to_finetune_sequence(X, Y, none_value="<PAD>")
-        self.target_type = SEQUENCE_LABELING
-        return self._finetune(X, Y, batch_size=batch_size)
+        arr_encoded = self._text_to_ids(X, Y=Y)
+        return self._training_loop(
+            arr_encoded,
+            Y=arr_encoded.labels,
+            batch_size=batch_size or self.config.batch_size
+        )
 
     def predict(self, X, max_length=None):
         """
@@ -52,7 +35,7 @@ class SequenceLabeler(BaseModel):
         :returns: list of class labels.
         """
         doc_subseqs, _ = indico_to_finetune_sequence(X)
-        arr_encoded = self._text_to_ids_with_labels(doc_subseqs)
+        arr_encoded = self._text_to_ids(doc_subseqs)
         labels = self._predict(X, max_length=max_length)
         all_subseqs = []
         all_labels = []
@@ -103,9 +86,35 @@ class SequenceLabeler(BaseModel):
         :returns: list of class labels.
         """
         doc_subseqs, _ = indico_to_finetune_sequence(X)
-        arr_encoded = self._text_to_ids_with_labels(doc_subseqs)
+        arr_encoded = self._text_to_ids(doc_subseqs)
         batch_probas = self._predict_proba(X, max_length=max_length)
         result = []
         for token_seq, proba_seq in zip(arr_encoded.tokens, batch_probas):
             result.append(list(zip(token_seq, proba_seq)))
         return result
+
+    def _target_placeholder(self):
+        return tf.placeholder(tf.int32, [None, self.config.max_length])  # classification targets
+
+    def _target_encoder(self):
+        return SequenceLabelingEncoder()
+
+    def _target_model(self, featurizer_state, targets, n_outputs, train=False, reuse=None, **kwargs):
+        return sequence_labeler(
+            hidden=featurizer_state['sequence_features'],
+            targets=targets, 
+            n_targets=n_outputs,
+            dropout_placeholder=self.do_dropout,
+            config=self.config,
+            train=train, 
+            reuse=reuse, 
+            **kwargs
+        )
+    
+    def _predict_op(self, logits, **kwargs):
+        label_idxs, _ = sequence_decode(logits, kwargs.get("transition_matrix"))
+        return label_idxs
+
+    def _predict_proba_op(self, logits, **kwargs):
+        _, label_probas = sequence_decode(logits, kwargs.get("transition_matrix"))
+        return label_probas
